f3ff6ace9fdd301a1fcd69f6d2b8e2e5b0c1b944
tomeshsahu <tomeshwarsahu1980@gmail.com>
2022-05-05 20:00:04 +0530

    [TOMESHWAR]ADD:- INT, DOUBLE & CHAR ARRAY PROGRAM

---
+++ GenericPracticeProblem/GenericArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPracticeProblem
+{
+    public class GenericArray
+    {
+      public static void toPrint(int[]intArray)
+        {
+            foreach(int element in intArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("---------------------------------");
+        }
+
+        public static void toPrint(double[] doubleArray)
+        {
+            foreach (double element in doubleArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("---------------------------------");
+        }
+
+        public static void toPrint(char[] charArray)
+        {
+            foreach (char element in charArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("---------------------------------");
+        }
+    }
+}
---
+++ GenericPracticeProblem/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using GenericPracticeProblem;
+
+
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("*********** Generic Practice Problem ***********");
+
+
+        bool check = true;
+        while (check)
+        {
+            Console.WriteLine("1.Sample Genric Problme");
+            Console.WriteLine("Enter Any option for Execute the Program");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    int[] intArray = { 1, 2, 3, 4, 5 };
+                    double[] doubleArray = { 1.1, 1.2, 1.3, 1.4, 1.5 };
+                    char[] charArray = { 'A', 'B', 'C', 'D', 'E' };
+
+                    GenericArray.toPrint(intArray);
+                    GenericArray.toPrint(doubleArray);
+                    GenericArray.toPrint(charArray);
+                    break;
+
+                case 0:
+                    check = false;
+                    break;
+
+            }
+        }
+    }
+}
+
